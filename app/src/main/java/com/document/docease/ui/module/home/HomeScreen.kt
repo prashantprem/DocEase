4b2f3ce7c2102a8f54bac3caace0b4ee9b50335c
Prashant <prashant.upadhyay@appyhigh.com>
2024-01-20 19:51:05 +0530

    added tab bar in home screen

--- app/src/main/java/com/document/docease/ui/module/home/HomeScreen.kt
+++ app/src/main/java/com/document/docease/ui/module/home/HomeScreen.kt
@@ -2,19 +2,29 @@ package com.document.docease.ui.module.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,13 +38,36 @@ fun HomeScreen() {
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)) {
-        var tabIndex by remember { mutableIntStateOf(0) }
+        var tabIndex by remember { mutableIntStateOf(1) }
         Image(painter = painterResource(id = R.drawable.ad_placeholder), contentDescription = "Native ad")
-        TabRow(selectedTabIndex = tabIndex, modifier = Modifier.padding(vertical = 16.dp)) {
+        TabRow(selectedTabIndex = tabIndex, modifier = Modifier.padding(vertical = 16.dp),
+            indicator = { tabPositions ->
+                Box(
+                    modifier = Modifier
+                        .tabIndicatorOffset(tabPositions[tabIndex])
+                        .height(3.dp)
+                        .width(8.dp)
+                        .clip(RoundedCornerShape(30.dp)) // clip modifier not working
+                        .padding(horizontal = 50.dp)
+                        .background(color = Color.Gray)
+
+                )
+
+            },
+             divider = {}) {
             repeat(3){
-                Tab(selected = tabIndex == it, onClick = { tabIndex = it }) {
+                Tab(selected = tabIndex == it, onClick = { tabIndex = it }, modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .height(30.dp)
+                    .background(
+                        color = colorResource(
+                            id = R.color.grey_bg
+                        )
+                    )) {
                     Image(painter = painterResource(id = tabs[it]), contentDescription = "Recent",
-                        modifier = Modifier.size(30.dp))
+                        modifier = Modifier
+                            .size(30.dp)
+                            .padding(6.dp))
                 }
             }
         }
