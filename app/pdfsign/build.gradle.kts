6a9856473383b7083b9d0bb56aaad49e6ce12eb3
Prashant <prashant.upadhyay@appyhigh.com>
2024-02-17 21:36:07 +0530

    release issues

--- app/pdfsign/build.gradle.kts
+++ app/pdfsign/build.gradle.kts
@@ -24,11 +24,11 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
+    }
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
     }
 }
 
@@ -38,17 +38,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.annotation:annotation:1.7.1")
+//    androidTestImplementation("androidx.annotation:annotation:1.7.1")
 
 
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.itextpdf:itextpdf:5.5.13")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.61")
-    implementation("org.bouncycastle:bcpkix-jdk15on:1.61")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.67")
     implementation("com.github.chrisbanes:PhotoView:2.0.0")
-    implementation("com.github.bumptech.glide:glide:4.9.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.castorflex.verticalviewpager:library:19.0.1")
     implementation("com.google.code.gson:gson:2.10.1")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,4 @@ plugins {
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.android.dynamic-feature") version "8.2.0" apply false
-
 }
